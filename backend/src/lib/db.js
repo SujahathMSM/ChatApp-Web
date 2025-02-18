f080aedcb40e4d5c786f15baa1d2207613d67d80
sujahathmsm <sujahathmhmd@gmail.com>
2025-02-18 22:05:57 +0530

    MongoDB setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database: " + conn.connection.host);
+  } catch (error) {
+    console.log("Error connecting to database: " + error.message);
+  }
+};
