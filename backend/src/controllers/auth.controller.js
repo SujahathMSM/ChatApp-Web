d3ad3880f37f009495ee6a7b6337526be2c854b5
sujahathmsm <sujahathmhmd@gmail.com>
2025-02-20 22:27:49 +0530

    SignUP-DONE

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,47 @@
-export const signUp = (req, res) => {
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signUp = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.find({ email });
+    if (!user) {
+      res.status(400).json({ message: "Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      //generate jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        photo: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
   } catch (error) {
-    console.log("An error occurred: " + error);
+    console.log("An error occurred during signUp: " + error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  res.send("Signup route");
 };
 export const logIn = (req, res) => {
   res.send("LogIn route");
