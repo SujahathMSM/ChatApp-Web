8e451801a7e5fa2f3d47c3f9d5f73a4811497e4d
sujahathmsm <sujahathmhmd@gmail.com>
2025-02-23 22:08:28 +0530

    Authentication full implemnted(BE)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,13 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 export const signUp = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
     if (password.length < 6) {
       return res
         .status(400)
@@ -43,9 +47,70 @@ export const signUp = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-export const logIn = (req, res) => {
-  res.send("LogIn route");
+export const logIn = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ message: "invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      message: "log in success",
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("An error occurred in controller: " + error.message);
+    res.status(500).json("Internal Server Error Occured");
+  }
 };
 export const logOut = (req, res) => {
-  res.send("LogOut route");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "logged out success" });
+  } catch (error) {
+    console.log("Error during logout: " + error.message);
+    res.status(400).json({ message: "Internal Server Error" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const profilePic = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      req.status(400).json({ message: "Profile Pic is Required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(201).json(updatedUser);
+  } catch (error) {
+    console.log("An error occurred during uploading image: " + error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("An error occurred during auth check: " + error.message);
+    res.status(500).json({ message: "internal server error" });
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import AuthRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", AuthRoutes);
 
 app.listen(PORT, () => {
